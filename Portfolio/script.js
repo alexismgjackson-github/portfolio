40abfe23700241fbafee86fcbed466570cbc7065
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-01-02 11:38:02 -0500

    portfolio redesign

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -9,26 +9,26 @@ const toggleSwitch = document.querySelector(
 const currentTheme = localStorage.getItem("theme");
 
 // if the current theme is true, set a "data-theme" attribute to document and "currentTheme" value
-// if the current theme is strictly equal to "dark" set the toggle switch checked to true
+// if the current theme is strictly equal to "light" set the toggle switch checked to true
 
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme === "dark") {
+  if (currentTheme === "light") {
     toggleSwitch.checked = true;
   }
 }
 
-// if toggle is checked set "data-theme" to "dark", else set "data-theme" to "light"
+// if toggle is checked set "data-theme" to "light", else set "data-theme" to "dark"
 // save both key/value data to local storage
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
   }
 }
 
