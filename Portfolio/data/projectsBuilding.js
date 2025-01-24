817e8b4e466be8008856be48239f1c422cfb6c63
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-01-24 13:43:24 -0500

    Add files via upload

--- Portfolio/data/projectsBuilding.js
+++ Portfolio/data/projectsBuilding.js
@@ -10,7 +10,7 @@ export const building = [
   {
     name: `Lexicon`,
     details: `This fully responsive app allows the user to search the definition
-              of a word. The user has the ability to toggle the app's dark/light
+              of a word. The user has the ability to toggle the dark/light
               mode theme and select the font style. The app receives data from
               the Merriam-Webster Dictionary API. This project will be built using React.`,
   },
