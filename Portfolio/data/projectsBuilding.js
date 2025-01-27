493283024fdf1b0a9129ba91cf8f404cfe35f466
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-01-27 15:57:45 -0500

    project section update

--- Portfolio/data/projectsBuilding.js
+++ Portfolio/data/projectsBuilding.js
@@ -4,8 +4,8 @@ export const building = [
     details: `This fully responsive app allows the user to keep a daily record
               of events and experiences. The user can toggle the dark/light mode
               theme for better visibility. The app utilizes Firebase
-              Authentication and Cloud Firestore to identify the user and store
-              their records. This project will be built using JavaScript.`,
+              Authentication and Firebase Realtime Database to identify the user 
+              and store their records. This project will be built using JavaScript.`,
   },
   {
     name: `Frontend Flash`,
