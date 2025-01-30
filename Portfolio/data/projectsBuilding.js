7c0ccd698363066824d88e2d0e092c16848e7311
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-01-30 14:29:01 -0500

    projects building update

--- Portfolio/data/projectsBuilding.js
+++ Portfolio/data/projectsBuilding.js
@@ -5,14 +5,8 @@ export const building = [
               of their job applications. The job applications can be sorted by 
               date, salary and status. The user can toggle the dark/light mode
               theme for better visibility. The app utilizes Firebase Realtime 
-              Database to store the data. This project will be built using React.`,
-  },
-  {
-    name: `Frontend Flash`,
-    details: `This fully responsive app allows the user to test their frontend development
-              knowledge utilizing flash cards. The user can choose which category to study. 
-              The questions are sourced from the "Frontend Developer Interview Questions" 
-              website. This project will be built using React.`,
+              Database to store the data. This project will be built using React
+              and Material UI.`,
   },
   {
     name: `Lexicon`,
@@ -20,5 +14,11 @@ export const building = [
               of a word. The user has the ability to toggle the dark/light
               mode theme and select the font style. The app receives data from
               the Merriam-Webster Dictionary API. This project will be built using React.`,
+  },
+  {
+    name: `Nomadic`,
+    details: `This fully responsive app allows the user to browse the listings of a select 
+              few camper vans for a duration. Images are sourced from the Unsplash website.
+              This project will be built using React and Material UI.`,
   },
 ];
