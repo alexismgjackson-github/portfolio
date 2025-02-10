423eeb2e51df9f5f7907627cab9720d7d9357a45
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-10 00:30:20 -0500

    Add files via upload

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -9,17 +9,18 @@ export const building = [
               and React Router 7.`,
   },
   {
+    name: `Gooddog`,
+    details: `This fully responsive app allows the user to book an appointment with
+              a fictional mobile dog grooming company. The price of the service is 
+              determined by age, breed and size. Images are sourced from the 
+              Unsplash website. This project is being built using React and React
+              Router 7.`,
+  },
+  {
     name: `Lexicon`,
     details: `This fully responsive app allows the user to search the definition
               of a word. The user has the ability to toggle the dark/light
               mode theme and select the font style. The app receives data from
               the Dictionary API. This project is being built using React.`,
-  },
-  {
-    name: `Nomadic`,
-    details: `This fully responsive app allows the user to browse a selection of 
-              camper vans from a fictional marketplace. The user can book a camper 
-              van for a duration of their choosing. Images are sourced from the Unsplash 
-              website. This project is being built using React and React Router 7.`,
   },
 ];
