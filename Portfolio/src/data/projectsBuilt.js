7ce53158595ed6c93c928a16ebebea2984271d0c
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-16 13:01:03 -0500

    js update

--- Portfolio/src/data/projectsBuilt.js
+++ Portfolio/src/data/projectsBuilt.js
@@ -3,7 +3,7 @@ export const built = [
     name: `Gooddog`,
     link: `https://alexis-jackson-gooddog.netlify.app`,
     details: `This fully responsive app allows the user to book an appointment with
-              a fictional dog grooming company. This project was built using React
+              a fictional dog grooming company. This project was built using React v19
               and React Router 7.`,
   },
   {
@@ -11,13 +11,13 @@ export const built = [
     link: `https://alexis-jackson-nanamis.netlify.app`,
     details: `This fully responsive site allows the user to order takeout
               directly from a fictional bakery. This project was built
-              using React, React Router 6 and LocalStorage.`,
+              using React v18, React Router 6 and LocalStorage.`,
   },
   {
     name: `Portfolio`,
     link: `#`,
     details: `This full responsive site showcases my projects and
-              experience. This project was built using JavaScript.`,
+              experience. This project was built using HTML, CSS and JavaScript.`,
   },
   {
     name: `Watcher`,
