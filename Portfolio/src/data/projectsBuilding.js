c9a13cac59bd6a1ed9886f33dad461a378e2ca0f
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-19 12:41:17 -0500

    js update

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -2,7 +2,7 @@ export const building = [
   {
     name: `AppTrack`,
     details: `This fully responsive CRUD app allows the user to keep a record
-              of their job applications. This project is being built using React v19, 
-              React Router 7, LocalStorage and the Logo Link API.`,
+              of their job applications. This project is being built using React, 
+              LocalStorage and the Logo Link API.`,
   },
 ];
