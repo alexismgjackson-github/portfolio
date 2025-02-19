535d026c0a0c026ac2b15ec470e4210a706d20a6
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-19 16:09:17 -0500

    js update

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -1,7 +1,7 @@
 export const building = [
   {
     name: `AppTrack`,
-    details: `This fully responsive CRUD app allows the user to keep a record
+    details: `This fully responsive CRUDish app allows the user to keep a record
               of their job applications. This project is being built using React, 
               LocalStorage and the Logo Link API.`,
   },
