c8b13542fa4964de6e0ac3cfb79fbf9b8ecbb2b5
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-20 14:24:04 -0500

    js update

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -1,11 +1,5 @@
 export const building = [
   {
-    name: `Champions`,
-    details: `This fully responsive app allows the user to search the details of
-    over 140 champions from League of Legends. This project is being built 
-    using React, React Router and the Riot API.`,
-  },
-  {
     name: `Lift`,
     details: `This fully responsive app allows the user to get tailored strength
     training workouts based on their needs. This project is being built 
