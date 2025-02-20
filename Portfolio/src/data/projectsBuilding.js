a64cdd332f5c4e7e16957ce1ddc965c05bc40376
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-20 12:01:42 -0500

    projects building js update

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -1,8 +1,20 @@
 export const building = [
   {
-    name: `AppTrack`,
-    details: `This fully responsive CRUDish app allows the user to keep a record
-              of their job applications. This project is being built using React, 
-              LocalStorage and the Logo Link API.`,
+    name: `Champions`,
+    details: `This fully responsive app allows the user to search more 
+    than 140 champions from League of Legends. This project is being built 
+    using React, React Router and the Riot API.`,
+  },
+  {
+    name: `Lift`,
+    details: `This fully responsive app allows the user to get tailored strength
+    training workouts based on their needs. This project is being built 
+    using React, React Router and the Claude AI.`,
+  },
+  {
+    name: `Yes Chef!`,
+    details: `This fully responsive app allows the user to get tailored recipes
+     based on the ingredients they have on hand. This project is being built 
+    using React, React Router and the Claude AI.`,
   },
 ];
