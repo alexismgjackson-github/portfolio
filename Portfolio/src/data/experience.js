0665a1db422af3c51a6eddc5d99f995c9011fdac
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-23 14:53:25 -0500

    experience update

--- Portfolio/src/data/experience.js
+++ Portfolio/src/data/experience.js
@@ -1,7 +1,7 @@
 export const experience = [
   {
     title: `Frontend Developer - Freelance`,
-    duration: `Nov 2024 - Present`,
+    duration: `Jan 2025 - Present`,
     details: `Implementing pixel-perfect, mobile-first UI/UX using HTML5, CSS3,
               JavaScript, and React. Optimize applications for maximum speed,
               scalability, accessibility and cross-browser compatibility. Utilize
