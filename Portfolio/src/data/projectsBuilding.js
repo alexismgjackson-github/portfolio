a7e55e1a7c6aa3f6647e6cbb4c82fa21edabaab8
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-02-23 20:28:35 -0500

    building update

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -6,7 +6,7 @@ export const building = [
     using React, React Router and the Claude AI.`,
   },
   {
-    name: `Yes Chef!`,
+    name: `Yes, Chef!`,
     details: `This fully responsive app allows the user to get tailored recipes
      based on the ingredients they have on hand. This project is being built 
     using React, React Router and the Claude AI.`,
