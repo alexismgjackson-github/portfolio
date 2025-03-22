170d52bf0fe9200b65d140ea8925f4e511d6b77c
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-03-22 10:54:39 -0400

    data js and css updated

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -1,19 +1,14 @@
 export const building = [
   {
+    name: `Check-in`,
+    details: `This fully responsive app allows the user to record daily mental health check-ins. 
+    This project is being built using React, React Router, Firebase Authentication and Firestore
+    Database.`,
+  },
+  {
     name: `Forecast`,
-    details: `This fully responsive app allows the user to search the weather forecast of their 
-    desired location. This project is being built using CSS, React, the OpenWeatherMap API and
+    details: `This fully responsive app allows the user to search the weather forecast of a desired city. 
+    This project is being built using CSS, React, the OpenWeatherMap API and
     LocalStorage.`,
-  },
-  {
-    name: `Lexicon`,
-    details: `This fully responsive app allows the user to search the definition of a word. 
-    This project is being built using CSS, React and the Free Dictionary API.`,
-  },
-  {
-    name: `Lift`,
-    details: `This fully responsive app allows the user to get tailored strength
-    training workouts based on their needs. This project is being built 
-    using React, React Router and the Claude AI.`,
   },
 ];
