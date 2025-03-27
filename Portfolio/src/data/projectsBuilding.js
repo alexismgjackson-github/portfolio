1d5a71696d2686d9f231e09988b5e2dffed6444b
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-03-26 22:21:41 -0400

    Add files via upload

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -5,4 +5,10 @@ export const building = [
     This project is being built using React, React Router, Firebase Authentication and Firestore
     Database.`,
   },
+  {
+    name: `Watcher V2`,
+    details: `This is an updated version of my pre-existing project "Watcher V1" with additional features. 
+    This project is being using CSS, React, React Router, The Movie Database API, Firebase Authentication
+    and Firestore Database.`,
+  },
 ];
