41096d015dd8fe4075720d95cebef3337a073a83
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-04-25 11:59:35 -0400

    projects update

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -1,11 +1,5 @@
 export const building = [
   {
-    name: `Check-In`,
-    details: `This fully responsive app allows the user to record daily mental health check-ins. 
-    This project is being built using React, React Router, Firebase Authentication and Firestore
-    Database.`,
-  },
-  {
     name: `Watcher V2`,
     details: `This is an updated version of my pre-existing project "Watcher V1" with additional features. 
     This project is being built using CSS, React, React Router, The Movie Database API, Firebase Authentication
--- Portfolio/src/data/projectsBuilt.js
+++ Portfolio/src/data/projectsBuilt.js
@@ -1,5 +1,13 @@
 export const built = [
   {
+    name: `Check-In`,
+    link: `https://alexis-jackson-checkin.netlify.app`,
+    code: `https://github.com/alexismgjackson-github/checkin`,
+    details: `This fully responsive app allows the user to record daily mental health check-ins. 
+    This project is being built using React, React Router, Firebase Authentication and Firestore
+    Database.`,
+  },
+  {
     name: `Gooddog`,
     link: `https://alexis-jackson-gooddog.netlify.app`,
     code: `https://github.com/alexismgjackson-github/gooddog`,
