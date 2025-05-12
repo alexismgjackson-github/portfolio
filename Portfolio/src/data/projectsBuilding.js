27fae8f0380b739e2aaff3f2a075c320c345f4f5
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-05-11 22:03:13 -0400

    update building

--- Portfolio/src/data/projectsBuilding.js
+++ Portfolio/src/data/projectsBuilding.js
@@ -1,17 +1,9 @@
 // An array of projects currently being worked on, with their names and details
 export const building = [
   {
-    // Apptrak allows users to track their job applications
-    name: `Apptrak`,
-    details: `This fully responsive app allows the user to track their job applications. 
-    This project is being built using CSS, React, React Router, Firebase Authentication, Firestore
-    Database and the Logo API.`,
-  },
-  {
-    // Watcher V2 is an updated version of the pre-existing "Watcher V1" with additional features
-    name: `Watcher V2`,
-    details: `This is an updated version of my pre-existing project "Watcher V1" with additional features. 
-    This project is being built using CSS, React, React Router, The Movie Database API, Firebase Authentication
-    and Firestore Database.`,
+    // Personal Portfolio (Version 2) website showcasing projects and experience
+    name: `Portfolio V2`,
+    details: `This fully responsive site showcases my projects and
+              experience. This project was built using CSS, React and React Router.`,
   },
 ];
